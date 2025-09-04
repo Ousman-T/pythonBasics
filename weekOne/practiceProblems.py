c0ea4524b993637a54f1a82173e7e83d8c2247e9
Ousman <otouray14@gmail.com>
2025-09-04 15:14:04 -0400

    Completed the intro python practice -Ousman

--- weekOne/practiceProblems.py
+++ weekOne/practiceProblems.py
@@ -56,3 +56,59 @@ def greet(name):
 
 greet("Momo")
 print("=====SECTION 11=====")
+
+def add_num(num1, num2):
+    """ Takes two parameters
+    adds them, an example of doctring for multi-line-comments """
+    return num1 + num2
+print(add_num(7,14))
+
+print("=====SECTION 12=====")
+help(greet("Momo"))
+print("=====SECTION 13=====")
+add = 5
+subtract = 11
+print(add + subtract)
+multiply = 15
+divide = 3
+print(multiply * divide)
+print (multiply / divide)
+print(subtract - multiply)
+
+print("=====SECTION 14=====")
+#lambda function to multiply numbers. anonymous 1 line function
+var = lambda a, b: a * b
+print(var(3, 5))
+print("=====SECTION 15=====")
+#utilized the int method to convert the float into an int. It doesn't round, rather it floors the number to it's whole int.
+float_num = 3.14
+int_num = int(float_num)
+print(int_num)
+print("=====SECTION 16=====")
+#in python you cannot concat a string and an int, int must be converted to string
+print("Age: " + "18")
+print("=====SECTION 17=====")
+score = 22
+if score < 50:
+    print("Low")
+elif 50 <= score < 80:
+    print("Average")
+else:
+    print("High")
+
+print("=====SECTION 18=====")
+x = None
+if x is None:
+    print("No value")
+else:
+    print("Contains value")
+
+print("=====SECTION 19=====")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+print(is_even(4))
+print(is_even(7))
