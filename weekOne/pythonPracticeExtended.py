3768f9b3cc56c5bc3ac243b6338e4d66dc241738
Ousman <otouray14@gmail.com>
2025-09-10 17:32:11 -0400

    Practice problems extended,continued -Ousman

--- weekOne/pythonPracticeExtended.py
+++ weekOne/pythonPracticeExtended.py
@@ -1,3 +1,5 @@
+from operator import truediv
+
 print("=====SECTION 1=====")
 age = 16
 is_student = False
@@ -22,4 +24,60 @@ def describe_type(value):
     elif type(value) == bool:
         print("Boolean")
 
-print(describe_type(25))
+print(describe_type("howdy"))
+
+print("=====SECTION 3=====")
+print(3+3.3+False)
+print("=====SECTION 4=====")
+x = 222
+if x >= 10 and x <= 20:
+    if x % 2 == 0:
+        print("Even")
+    elif x % 2 == 1:
+        print("Odd")
+else:
+    print("Number not within range")
+
+print("=====SECTION 5=====")
+logged_in = False
+admin = False
+if logged_in == True and admin == True:
+    print("Welcome admin")
+elif logged_in == True and admin == False:
+    print("Welcome user")
+else:
+    print("Access Denied")
+
+print("=====SECTION 6=====")
+def get_username(name):
+    if name == None:
+        print("Guest")
+    else:
+        print(name)
+print(get_username(None))
+
+print("=====SECTION 7=====")
+#Can not add a string to a bool or int because Python will treat it as if we are trying to do string concatenation
+#print("38" + False)
+#print(3.33 + "11")
+
+print("=====SECTION 8=====")
+def greet(name, is_morning):
+    if is_morning == True:
+        print(f"Good morning, {name}!")
+    else:
+        print(f"Hello, {name}!")
+print(greet("Ousman", False))
+
+print("=====SECTION 9=====")
+username = ""
+display_name = username or "Guest"
+print(display_name)
+
+print("=====SECTION 10=====")
+def is_divisible(x,y):
+    if x%y == 0:
+        return True
+    else:
+        return False
+print(is_divisible(20,5))
